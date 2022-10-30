d3cee9d0238e33fd1473ecd6c7a975d9aafdc117
marc0dev0417 <98348202+marc0dev0417@users.noreply.github.com>
2022-10-30 21:30:51 +0100

    Add files via upload

---
+++ Food.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON
+{
+    internal class Food
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+        public Food(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+         
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using JSON;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+class Program
+{
+  public static void Main()
+    {
+        Food food = new Food(1, "Hotdog");
+
+        var myJson = JsonSerializer.Serialize(food);
+
+        Console.WriteLine(myJson);
+
+        File.WriteAllText("C:\\Users\\Marco Alonso Benegas\\Desktop\\jsonC#.txt", myJson);
+
+        string textJson = File.ReadAllText("C:\\Users\\Marco Alonso Benegas\\Desktop\\jsonC#.txt");
+
+        Food foodToObject = JsonSerializer.Deserialize<Food>(textJson);
+    }
+}
